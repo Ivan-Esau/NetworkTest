025b061aedc056ef351d7d4ec77c963759e7e499
Ivan-Esau <esauivan17@gmail.com>
2024-07-07 12:54:02 +0200

    Win method fix

--- src/logic/GameLogic.java
+++ src/logic/GameLogic.java
@@ -90,7 +90,7 @@ public class GameLogic {
 
     private boolean checkRows() {
         for (int i = 0; i < 3; i++) {
-            if (board[i][0] != '-' && board[i][0] == board[i][1] && board[i][1] == board[i][2] && (board[i][0] == 'X' || board[i][0] == 'O')) {
+            if (board[i][0] != '-' && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
                 return true;
             }
         }
@@ -99,7 +99,7 @@ public class GameLogic {
 
     private boolean checkColumns() {
         for (int i = 0; i < 3; i++) {
-            if (board[0][i] != '-' && board[0][i] == board[1][i] && board[1][i] == board[2][i] && (board[0][i] == 'X' || board[0][i] == 'O')) {
+            if (board[0][i] != '-' && board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
                 return true;
             }
         }
@@ -107,10 +107,10 @@ public class GameLogic {
     }
 
     private boolean checkDiagonals() {
-        if (board[0][0] != '-' && board[0][0] == board[1][1] && board[1][1] == board[2][2] && (board[0][0] == 'X' || board[0][0] == 'O')) {
+        if (board[0][0] != '-' && board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
             return true;
         }
-        if (board[0][2] != '-' && board[0][2] == board[1][1] && board[1][1] == board[2][0] && (board[0][2] == 'X' || board[0][2] == 'O')) {
+        if (board[0][2] != '-' && board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
             return true;
         }
         return false;
